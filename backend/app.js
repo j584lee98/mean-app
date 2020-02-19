fcd0f84a8c7181c8adc540fcd865a96e39f8be46
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-02-19 00:57:56 -0500

    set up node/express

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,51 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+  )
   next();
 })
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'asdfsadf',
+      title: 'First post',
+      content: 'First post content'
+    },
+    {
+      id: 'lgkegk',
+      title: 'Second post',
+      content: 'Second post content'
+    },
+    {
+      id: 'qwetk',
+      title: 'Third post',
+      content: 'Third post content'
+    },
+  ];
+  res.status(200).json({
+    message: 'Post fetched successfully',
+    posts: posts
+  });
+});
+
+module.exports = app;
