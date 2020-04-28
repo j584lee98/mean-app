2e426a4eb7c313359e3c2488d7eda822c315095a
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-04-28 01:57:53 -0400

    user model and routes for login and signup

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const postRoutes = require('./routes/posts')
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/posts", postRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
